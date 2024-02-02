4739fd2bfe0c6df52bac91419e5bafd79639b518
Jenny Tsai <jennytsai@gwu.edu>
2024-02-01 23:53:42 -0500

    dataset and preprocessing
    dataset and preprocessing

---
+++ code/main_code/data_preprocessing.py
@@ -0,0 +1,33 @@
+#%%
+import pandas as pd
+
+df_20 = pd.read_spss('data/CABG 2020 Original.sav')
+df_19 = pd.read_spss('data/CABG 2019 Original.sav')
+df_18 = pd.read_spss('data/CABG 2018.sav')
+
+print(f'2020 size:{df_20.shape}')
+print(f'2019 size:{df_19.shape}')
+print(f'2018 size:{df_18.shape}')
+
+print(f'Total sample size:{df_20.shape[0]+df_19.shape[0]+df_18.shape[0]}')
+print(f'Total number of features: 274 - 276')
+
+
+#%%
+
+df_20.to_csv('CABG_2020.csv')
+
+#%%
+for f in df_20.columns:
+    print(f)
+
+#%%     
+features = ['SEX','RACE_NEW','HEIGHT','WEIGHT','INOUT','AGE',
+            'ANESTHES','DIABETES','SMOKE','DYSPNEA','FNSTATUS2',
+            'HXCOPD','ASCITES','HXCHF','HYPERMED','DIALYSIS',
+            'DISCANCR','STEROID','WTLOSS','BLEEDIS']
+df_20_s = df_20[features]
+print(df_20_s.columns)
+
+#%%
+print(df_20['OTHBLEED'].value_counts())
