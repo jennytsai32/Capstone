5bd07e8ea739b3d8d8c780b693408af7fb11cca9
Jichong Wu <jichongwu2011@gmail.com>
2024-02-20 01:02:17 -0500

    Updated model code

---
+++ code/component/class_baseline_decision_tree.py
@@ -0,0 +1,153 @@
+# build a class of basic classification models for our baseline model construction
+# including Decision Tree, SVM, LogisticRegression, and XGBoost
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, roc_curve, roc_auc_score, mean_squared_error, f1_score
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
+from xgboost import XGBClassifier
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class DecisionTree:
+
+    def __init__(self, df, target, test_size, random_state, k_folds, criterion, max_depth, min_samples_leaf):   # usually criterion = 'gini' or 'entropy', max_depth=3, min_samples_leaf=5
+        self.df = df
+        self.target = target
+        self.test_size = test_size
+        self.random_state = random_state
+        self.k_folds = k_folds
+        self.model_name = 'Decision Tree - ' + criterion
+        self.parameters = 'max_depth='+str(max_depth) +', min_samples_leaf='+str(min_samples_leaf)
+
+        self.X = df.drop([target], axis=1).values
+        self.y = df[target].values
+        self.X_train = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)[0]
+        self.X_test = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)[1]
+        self.y_train = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)[2]
+        self.y_test = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)[3]
+
+        self.class_names = self.df[self.target].unique()
+        self.feature_names = self.df.drop([self.target], axis=1).columns
+
+        # get y_pred
+        self.model = DecisionTreeClassifier(criterion=criterion, random_state=random_state, max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+        self.model.fit(self.X_train, self.y_train)
+        self.y_pred = self.model.predict(self.X_test)
+
+        # build a model results table
+        self.results_table = pd.DataFrame(columns=['Model Name', 'Parameters', 'Target', 'Mean Accuracy (10 folds)', 'RMSE', 'F1-score'])
+
+    def Predict(self):
+        self.model.fit(self.X_train, self.y_train)
+
+        # # make predictions
+        # y_pred = self.model.predict(self.X_test)
+
+        # display results
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print(f'Y-test prediction: {self.y_pred}')
+
+
+    def Report(self):
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print('Report: ' + '\n',
+              classification_report(self.y_test, self.y_pred))
+
+    def Accuracy(self):
+        # perform cross validation
+        kf = KFold(n_splits=self.k_folds, shuffle=True, random_state=self.random_state)
+        scores = cross_val_score(self.model, self.X, self.y, cv=kf)
+        mean_accuracy = scores.mean() * 100
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print(f'Model Accuracy (mean of {self.k_folds} folds): {mean_accuracy}')
+
+
+    def RMSE(self):
+        rmse = mean_squared_error(self.y_test, self.y_pred, squared=False)
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print(f'RMSE: {rmse: .3f}')
+
+    def Confusion_Matrix(self):
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print('Confusion Matrix: ' + '\n', confusion_matrix(self.y_test, self.y_pred))
+
+    def Confusion_Matrix_Plot(self):
+        print('Plot confusion matrix: ')
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+
+        # plot confusion matrix
+        conf_matrix = confusion_matrix(self.y_test, self.y_pred)
+        df_conf_matrix = pd.DataFrame(conf_matrix, index=self.class_names, columns=self.class_names)
+
+        plt.figure(figsize=(10, 10))
+        hm = sns.heatmap(df_conf_matrix, cbar=False, annot=True, square=True, fmt='d', annot_kws={'size': 20},
+                         yticklabels=df_conf_matrix.columns, xticklabels=df_conf_matrix.columns)
+        hm.yaxis.set_ticklabels(hm.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=20)
+        hm.xaxis.set_ticklabels(hm.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=20)
+        plt.ylabel('True label', fontsize=20)
+        plt.xlabel('Predicted label', fontsize=20)
+        plt.tight_layout()
+        plt.show()
+
+    def Decision_Tree_Plot(self):
+        # plot the tree
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print('Plot the decision tree: ')
+        plt.figure(figsize=(15, 10))
+        tree.plot_tree(self.model, filled=True, feature_names=self.feature_names, class_names=['Transfusions','No'], rounded=True, fontsize=14)
+        plt.show()
+
+    def ROC_AUC_Plot(self):
+        # Plot ROC Area Under Curve
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print('Plot ROC Aarea Under Curve: ')
+
+        y_pred_proba = self.model.decision_function(self.X_test)
+        fpr, tpr, _ = roc_curve(self.y_test, y_pred_proba)
+        auc = roc_auc_score(self.y_test, y_pred_proba)
+
+        plt.figure(figsize=(10, 10))
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve (area = %0.2f)' % auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    def Display_Model_Results(self):
+        print('Target: ', self.target)
+        print('Model: ', self.model_name)
+        print('Model Results:' + '\n')
+        report = classification_report(self.y_test, self.y_pred, output_dict=True)
+
+        kf = KFold(n_splits=self.k_folds, shuffle=True, random_state=self.random_state)
+        scores = cross_val_score(self.model, self.X, self.y, cv=kf)
+        mean_accuracy = scores.mean() * 100
+        rmse = mean_squared_error(self.y_test, self.y_pred, squared=False)
+
+        f1 = report['macro avg']['f1-score']
+        self.results_table.loc[len(self.results_table.index)] = [self.model_name, self.parameters, self.target, mean_accuracy, rmse, f1]
+
+        return self.results_table
+
--- code/main_code/main_draft_jw.py
+++ code/main_code/main_draft_jw.py
@@ -44,20 +44,62 @@ standardize(df)
 # STEP2. Baseline models and results (before feature selection)               ||
 # ==============================================================================
 
+# import the baseline models class
+from class_baseline_decision_tree import DecisionTree
+from class_baseline_svm import SVM
+from class_baseline_logistic import LogReg
+from class_baseline_xgboost import XGB
+
 # ALTERNATIVE - import clean data
 df = pd.read_csv(r'https://raw.githubusercontent.com/jennytsai32/Capstone/master/code/main_code/CABG_20_recoded.csv',index_col=0)
 df = df.drop(['PUFYEAR','AGE','BMI'], axis=1)
 
+#===============================================
+# Model 1: call Decision Tree model class
+#===============================================
 
-from class_baseline_classification_models import BaselineClassificationModels
+# initiate the model
+model_decision_tree = DecisionTree(df,'OTHBLEED',0.3,100, 5,'gini', 3, 5)
 
-# pull the models from BasicClassificationModels class
-data = BaselineClassificationModels(df,'OTHBLEED',0.3,100, 5)
+# run Decision Tree model and display results
+model_decision_tree.Predict()
+model_decision_tree.Report()
+model_decision_tree.Accuracy()
+model_decision_tree.RMSE()
+model_decision_tree.Confusion_Matrix()
+model_decision_tree.Confusion_Matrix_Plot()
+model_decision_tree.Decision_Tree_Plot()
+model_decision_tree.ROC_AUC_Plot()
+model_decision_tree.Display_Model_Results()
 
-# run different models and compare results
-data.Decision_Tree('gini',3,5)
-data.Decision_Tree('entropy',3,5)
-data.SVM('linear',1.0,0)
-data.SVM('rbf',1.0,0.2)
-data.LogReg()
-data.XGBoost(100, 0.3)
+#===============================================
+# Model 2: call Decision Tree model class
+#===============================================
+# initiate the model
+model_decision_tree2 = DecisionTree(df,'OTHBLEED',0.3,100, 5,'entropy', 3, 5)
+model_decision_tree2.Display_Model_Results()
+
+#===============================================
+# Model 3: call SVM model class
+#===============================================
+model_SVM = SVM(df,'OTHBLEED',0.3,100, 5,'linear',1.0,0)
+model_SVM.Display_Model_Results()
+
+#===============================================
+# Model 4: call SVM model class
+#===============================================
+model_SVM2 = SVM(df,'OTHBLEED',0.3,100, 5,'rbf',1.0,0.2)
+model_SVM2.Display_Model_Results()
+
+#===============================================
+# Model 5: call Logistic Regression model class
+#===============================================
+model_log = LogReg(df,'OTHBLEED',0.3,100, 5)
+model_log.Display_Model_Results()
+
+#===============================================
+# Model 6: call XGBoost model class
+#===============================================
+model_xgb = XGB(df,'OTHBLEED',0.3,100, 5,100, 0.3)
+model_xgb.Display_Model_Results()
+
